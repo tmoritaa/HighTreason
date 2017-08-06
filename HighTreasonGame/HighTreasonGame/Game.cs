8e44a00bf3c3f4973b8dcd32ceac097e82a4da0c
tmoritaa <tatsuya.ahad@gmail.com>
2017-08-06 17:34:33 -0400

    Downgraded code to .NET 3.5 for Unity support

--- HighTreasonGame/HighTreasonGame/Game.cs
+++ HighTreasonGame/HighTreasonGame/Game.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 using HighTreasonGame.GameStates;
 using HighTreasonGame.ChoiceHandlers;
